495f4e66578f18258784a51856d3f00218194cd5
astellav <astellav@health.usf.edu>
2016-07-01 13:51:13 -0400

    Initial commit of Program.cs

--- TestGithub/Program.cs
+++ TestGithub/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace TestGithub
 {
@@ -10,6 +7,7 @@ namespace TestGithub
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, Git.");
         }
     }
 }
